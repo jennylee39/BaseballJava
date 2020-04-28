50e00ba82819453b0a6e2f7cc0e42896e979bea3
jennylee39 <zxxexn@gmail.com>
2020-04-28 11:24:40 +0900

    Baseball Java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    static int MAX_NUMBER = 10;
 
     public static void main(String[] args) {
         // 정답을 생성한다. (0 - 9 사이의 중복되지 않는 3개의 숫자)
@@ -12,10 +13,12 @@ public class Main {
         // base class library(API) vs. 3rd-party library
         Random random = new Random();
 
+        MAX_NUMBER = 9;
+
         while (true) {
-            answer0 = random.nextInt(10);
-            answer1 = random.nextInt(10);
-            answer2 = random.nextInt(10);
+            answer0 = random.nextInt(MAX_NUMBER);
+            answer1 = random.nextInt(MAX_NUMBER);
+            answer2 = random.nextInt(MAX_NUMBER);
 
             if (answer0 != answer1 && answer1 != answer2 && answer2 != answer0)
                 break;
@@ -24,6 +27,7 @@ public class Main {
         System.out.print(answer0 + " ");
         System.out.print(answer1 + " ");
         System.out.print(answer2 + " ");
+        System.out.println();
 
         // 추측을 입력한다.
         int tryCount = 0;
@@ -72,7 +76,7 @@ public class Main {
             else
                 out++;
 
-            System.out.println("S : " + strike + "B : " + ball + "O : " + out);
+            System.out.println("S : " + strike + " B : " + ball + " O : " + out);
 
             // 3S가 아니면 돌아간다.
             if (strike == 3)
